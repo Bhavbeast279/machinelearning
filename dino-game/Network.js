f8bf23d1ede61a1447205db52368a6423e269034
Bhavbeast279 <54278111+Bhavbeast279@users.noreply.github.com>
2019-08-23 15:05:28 -0400

    face tracking game

--- dino-game/Network.js
+++ dino-game/Network.js
@@ -1,5 +1,55 @@
 class NeuralNetwork {
-    constructor() {
+    constructor(a, b, c, d) {
+        
+        if(a instanceof tf.Sequential) {
+            this.model = a;
+            this.input_nodes = b;
+            this.hidden_nodes = c;
+            this.output_nodes = d;
+        }else{
+            this.input_nodes = a;
+            this.hidden_nodes = b;
+            this.output_nodes = c;
+            this.model = this.create_model();
+        }
+    }
 
+    create_model() {
+        // Unsurpervised Dense Neural Network
+        const model = tf.sequential();
+        const hidden = tf.layers.dense({
+            units: this.hidden_nodes,
+            inputShape: [this.input_nodes],
+            activation: 'sigmoid'
+        });
+        model.add(hidden);
+        const hidden2 = tf.layers.dense({
+            units: this.hidden_nodes,
+            activation: 'sigmoid'
+        });
+        model.add(hidden2);
+        const output = tf.layers.dense({
+            units: this.output_nodes,
+            activation: 'softmax'
+        });
+        model.add(output);
+
+        return model;
+    }
+
+    dispose(){
+        this.model.dispose();
+    }
+
+    predict(inputs) {
+        return tf.tidy(() => {
+            const xs = tf.tensor2d([inputs]); // [[]] lives in this GPU
+
+            const ys = this.model.predict(xs); // Predict using our inputs
+
+            const outputs = ys.dataSync(); // Convert Tensor to Array
+
+            return outputs;
+        });
     }
 }
--- dino-game/sketch.js
+++ dino-game/sketch.js
@@ -1,10 +1,111 @@
 let ground;
+let cacti = [];
+let dinos = [];
+
+let spawnCactusFrame;
+
+let score = 0;
+let highScore = 0
+
+
 function setup() {
+    tf.setBackend('cpu');
     createCanvas(640, 480)
+
     ground = new Ground();
+
+    spawnCactusFrame = 40;
+    cacti.push(new Cactus());
+
+    first_generation();
     
 }
+
 function draw() {
     background(255);
+
+    score += 1;
+
+    if(score > highScore) {
+        highScore = score;
+    }    
+
+    push();
+    fill(0);
+    textSize(18);
+    textAlign(RIGHT)
+    text(score, width, 30);
+    text("HI: " + highScore, width, 55);
+    pop();
+
     ground.show();
+
+    if(frameCount == spawnCactusFrame){
+        cacti.push(new Cactus());
+        spawnCactusFrame += int(random(40, 100));
+    }
+
+    for(let i = 0; i < cacti.length; i++) { 
+
+        if(cacti[i].pos.x < - cacti[i].width) {
+            cacti.shift();
+        }
+
+        cacti[i].update();
+        cacti[i].show();
+    }
+    
+    let allDead = true;
+
+    for (let i = 0; i < dinos.length; i++){
+        if(dinos[i].playerControlled) {
+            if(keyIsDown(DOWN_ARROW)) {
+                dinos[i].duck();
+            }else{
+                dinos[i].unDuck();  
+            }
+            if (keyIsDown(UP_ARROW)) {
+                dinos[i].jump();
+            }
+        }
+        if(dinos[i].isAlive) {
+            allDead = false;
+            dinos[i].update(getClosesCactus(dinos[i]));                              
+            dinos[i].show();
+        }
+    }
+
+    if(allDead) {
+        next_generation();
+    }
+}
+
+function getClosesCactus(dino) {
+
+    let closestIndex = 0;
+
+    while(cacti[closestIndex].pos.x + cacti[closestIndex].fullWidth/2 < dino.pos.x - dino.width/2) {
+        closestIndex++;
+    }
+
+    return cacti[closestIndex];
+    
+}
+
+function first_generation() {
+    dinos = [];
+   // dinos.push(new Dino(false, new NeuralNetwork(2, 4, 3)));
+   dinos.push(new Dino (true))
+}
+
+function next_generation() {
+    dinos = [];
+   // dinos.push(new Dino(false, new NeuralNetwork(2, 4, 3)));
+    dinos.push(new Dino (true))
+
+    spawnCactusFrame = frameCount + 40;
+    cacti = [];
+    cacti.push(new Cactus());
+
+    score = 0;
 }
